470d0a8bf2b10e4d21bcce8794173b08bd40cd38
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-11-02 19:51:53 +0300

    Initial Commit

---
+++ Swift-Network/Swift_NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_NetworkApp.swift
+//  Swift-Network
+//
+//  Created by Bekir Berke Yılmaz on 2.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
